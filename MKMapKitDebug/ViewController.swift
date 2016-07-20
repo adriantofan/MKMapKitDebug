b937b7303230e0078dafe739902bea8e9dcf4191
Adrian Tofan <adrian@tofan.co>
2016-07-20 10:57:18 +0200

    added mapkit

--- MKMapKitDebug/ViewController.swift
+++ MKMapKitDebug/ViewController.swift
@@ -7,21 +7,31 @@
 //
 
 import Cocoa
+import MapKit
 
 class ViewController: NSViewController {
 
+  @IBOutlet weak var mapView: MKMapView!
   override func viewDidLoad() {
     super.viewDidLoad()
+    mapView.delegate  = self
+    print(mapView.centerCoordinate)
 
     // Do any additional setup after loading the view.
   }
-
+  func paste(sender:AnyObject){
+    let pasteboard = NSPasteboard.generalPasteboard()
+    let str = pasteboard.stringForType(NSPasteboardTypeString)
+    print(str)
+  }
+  
   override var representedObject: AnyObject? {
     didSet {
     // Update the view, if already loaded.
     }
   }
-
+}
+extension ViewController : MKMapViewDelegate{
 
 }
 
