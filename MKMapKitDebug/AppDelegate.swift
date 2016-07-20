6939fc41f20fce1cb82cae6bc73bcf5871b97fe5
Adrian Tofan <adrian@tofan.co>
2016-07-20 10:27:23 +0200

    Initial Commit

---
+++ MKMapKitDebug/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MKMapKitDebug
+//
+//  Created by Adrian Tofan on 20/07/2016.
+//  Copyright © 2016 tofan.co. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ MKMapKitDebug/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MKMapKitDebug
+//
+//  Created by Adrian Tofan on 20/07/2016.
+//  Copyright © 2016 tofan.co. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: AnyObject? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
